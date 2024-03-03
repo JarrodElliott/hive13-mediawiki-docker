64b3537404bec86d8f3f5d1c6c97e2c7f480d495
Jarrod Elliott <jarrodaelliott@gmail.com>
2024-03-03 10:33:12 -0500

    Add more missing files

---
+++ extensions/Widgets/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a0fb45f0cf80d2ac4709463893abec3622924f59',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a0fb45f0cf80d2ac4709463893abec3622924f59',
+    ),
+    'smarty/smarty' => 
+    array (
+      'pretty_version' => 'v4.3.5',
+      'version' => '4.3.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e0cbbdf6ea21768d0194e59d2f8c2e20d5f0868c',
+    ),
+  ),
+);
